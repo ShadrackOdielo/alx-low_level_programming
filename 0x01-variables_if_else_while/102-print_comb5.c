0d7237f57c34e4201b53145a85f6f310d474cf37
ShadrackOdielo <shadrackodielo@gmail.com>
2022-12-10 01:55:21 +0300

    update

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- The entry point
+ * Return: zero
+ */
+int main(void)
+{
+	int n, p;
+
+	for (p = 0; p <= 9; p++)
+	{
+		putchar(p + '0');
+		for (n = 0; n <= 9; n++)
+			putchar(n + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
