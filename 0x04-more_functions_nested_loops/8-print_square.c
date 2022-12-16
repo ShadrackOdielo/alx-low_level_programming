c8f79c1f90e0ef8ca5515d6087f329ebd553ee8d
ShadrackOdielo <shadrackodielo@gmail.com>
2022-12-17 02:14:29 +0300

    update

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_square - a function that prints a square, followed by a new line
@@ -7,7 +7,7 @@
  */
 void print_square(int size)
 {
-	int co, ro;
+	int col, row;
 
 	if (size <= 0)
 	{
@@ -15,10 +15,10 @@ void print_square(int size)
 	}
 	else
 	{
-		for (co = 1; co <= size; co++)
+		for (col = 1; col <= size; col++)
 		{
 			_putchar('#');
-			for (ro = 2; ro <= size; ro++)
+			for (row = 2; row <= size; row++)
 			{
 				_putchar('#');
 			}
