fd366d28d6b1b687ff36b1dbf6757bf88e228539
ShadrackOdielo <shadrackodielo@gmail.com>
2022-12-20 05:27:49 +0300

    update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,14 +13,15 @@ void print_array(int *a, int n)
 {
 	int j;
 
-	if (n == 0)
+	for (j = 0; j < n; j++)
 	{
-		return;
+		printf("%d", a[j]);
+
+		if (j != (n - 1))
+		{
+			printf(", ");
+		}
 	}
-	for (j = 0; j < n - 1; j++)
-	{
-		printf("%d, ", a[j]);
-	}
-	printf("%d", a[n - 1]);
+
 	printf("\n");
 }
