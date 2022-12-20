cee3865b6415ec3421eb7dd64fa87b75c766550f
ShadrackOdielo <shadrackodielo@gmail.com>
2022-12-20 04:59:07 +0300

    update

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,11 +13,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-
-	do {
+	for (; src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
-		i++;
-	} while (src[i] != '\0');
-
+	}
 	return (dest);
 }
