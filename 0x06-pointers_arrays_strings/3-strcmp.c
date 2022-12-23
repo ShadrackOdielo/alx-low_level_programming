fb87aff885df4a9a22db501d4ac36639e4ba6728
ShadrackOdielo <shadrackodielo@gmail.com>
2022-12-23 03:51:04 +0300

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,8 +13,8 @@ int _strcmp(char *s1, char *s2)
 
 	for (p = 0; s1[i] != '\0' && s2[i] != '\0'; p++)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (s1[p] != s2[p])
+			return (s1[p] - s2[p]);
 	}
 	return (0);
 }
