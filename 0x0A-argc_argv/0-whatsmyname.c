e90f953a58cd9bfc81178b79c823cda7f10fa8d7
ShadrackOdielo <shadrackodielo@gmail.com>
2023-01-10 00:01:04 +0300

    recursion assignment

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  * @argv: array of command-line arguments.
  * Return: always 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	argc = 0;
-
-	printf("%s\n", argv[argc]);
+	printf("%s\n", argv[0]);
 	return (0);
+}
+
